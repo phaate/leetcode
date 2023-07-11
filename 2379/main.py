906d22ba216929b97da99a1776121b2c8fdbd333
phaate <lukasz.swiderski@hotmail.com>
2023-07-11 03:34:15 +0200

    2379 resolved

--- 2379/main.py
+++ 2379/main.py
@@ -1,6 +1,27 @@
 class Solution:
     def minimumRecolors(self, blocks: str, k: int) -> int:
+        if k == 0 or k*'B' in blocks:
+            return 0
+        else:
+            i = 0
+            min = k
+            for i in range(len(blocks)-(i+k-1)):
+
+                curr = 0
+                for letter in blocks[i:i + k]:
+                    if letter == 'W':
+                        curr += 1
+                if curr < min:
+                    min=curr
+                i+=1
+        return min
+
+
+        
+
+
 
 
 if __name__ == '__main__':
-    pass
+    s = Solution()
+    print(s.minimumRecolors("WBBWWBBWBW", 7))
